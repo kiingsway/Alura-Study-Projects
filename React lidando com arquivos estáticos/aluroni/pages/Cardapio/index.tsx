67316dc861049c0281035e86dc83ca12179ed3fa
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2022-07-10 18:17:23 -0300

    Novo curso: React lidando com arquivos estáticos. Aula 01

---
+++ React lidando com arquivos estáticos/aluroni/pages/Cardapio/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Cardapio.module.scss";
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className="menu">
+        xxx
+      </nav>
+    </main>
+  )
+}
