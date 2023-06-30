b4136d70d2bca765a3ffaa9a50c729c07a74abff
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2023-06-30 04:52:49 -0300

    nextjs course

--- Nextjs explorando o framework/nextjs-course/pages/faq.tsx
+++ Nextjs explorando o framework/nextjs-course/pages/faq.tsx
@@ -6,10 +6,9 @@ export async function getStaticProps() {
 
   const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
 
-  const faq = await axios({
-    method: 'get',
-    url: FAQ_API_URL
-  }).then(resp => resp.data)
+  const faq = await axios
+    .get(FAQ_API_URL)
+    .then(resp => resp.data)
 
   return { props: { faq } }
 
@@ -24,11 +23,11 @@ export default function FAQPage({ faq }) {
         Ir para a home
       </Link>
       <ul>
-        {faq.map((q: any) => (
-          <li key={q.question}>
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
             <article>
-              <h1>{q.question}</h1>
-              <p>{q.answer}</p>
+              <h1>{question}</h1>
+              <p>{answer}</p>
             </article>
           </li>
         ))}
